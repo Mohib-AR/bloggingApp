adfa93715a5b5077698a6ef525f14b1e5ac06cfa
Mohib-AR <mohibali170@gmail.com>
2024-10-23 19:05:15 +0500

    correcting errors for deployment

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -3,10 +3,19 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": { target: "http://localhost:3000", secure: false },
-    },
+  server:
+    command === "serve"
+      ? {
+          proxy: {
+            "/api": {
+              target: "http://localhost:3000",
+              secure: false,
+            },
+          },
+        }
+      : {},
+  build: {
+    outDir: "dist", 
   },
   plugins: [react()],
 });
