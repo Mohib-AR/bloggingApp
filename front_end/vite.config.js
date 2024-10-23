2903609155b3c932d8eeda21b884092e7a5f3c3b
Mohib-AR <mohibali170@gmail.com>
2024-10-23 19:09:54 +0500

    modifying vite configuration file

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -2,20 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server:
-    command === "serve"
-      ? {
-          proxy: {
-            "/api": {
-              target: "http://localhost:3000",
-              secure: false,
+export default defineConfig(({ command }) => {
+  return {
+    server:
+      command === "serve"
+        ? {
+            proxy: {
+              "/api": {
+                target: "http://localhost:3000",
+                secure: false,
+              },
             },
-          },
-        }
-      : {},
-  build: {
-    outDir: "dist", 
-  },
-  plugins: [react()],
+          }
+        : {},
+    build: {
+      outDir: "dist", // Ensure that the build output is directed to the "dist" folder
+    },
+    plugins: [react()],
+  };
 });
